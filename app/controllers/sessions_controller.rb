217b942a6dc37d5898029c622542f3ca7ec6fe93
parisdolfman <parisdolfman@gmail.com>
2021-04-07 09:58:04 -0400

    commit tree 8c6d50307d8dc1d7fd4d5535e9291c9d5fabb861

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+
+    return redirect_to '/login' unless @user.authenticate(params[:user][:password])
+    
+    session[:user_id] = @user.id
+    redirect_to '/'
+end
+
+
 end
